b9b398bf9f02a689fd2bf260e53f5516dcc64143
@anhnv99 <vietanhhappy99@gmail.com>
2022-05-05 08:25:06 +0700

    lesson 50

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,47 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './Specialty.scss';
 import { FormattedMessage } from 'react-intl';
 import Slider from "react-slick";
 
-// import specialtyImg from "../../../assets/specialty/co-xuong-khop.jpg"
+// import specialtyImg from "../../../assets/HandBook/co-xuong-khop.jpg"
 
-class Specialty extends Component {
+class HandBook extends Component {
 
 
     render() {  
         
         return (
-            <div className="section-share section-specialty">
+            <div className="section-share section-handbook">
                 <div className="section-container">
                     <div className="section-header">
-                        <span className="title-section">Chuyên khoa phổ biến</span>
+                        <span className="title-section">Cẩm nang</span>
                         <button className="btn-section">xem thêm</button>
                     </div>
                     <div className="section-body">
                         <Slider {...this.props.settings}>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ Xương khớp 1</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ Xương khớp 2</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ Xương khớp 3</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ Xương khớp 4</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ Xương khớp 5</div>
                             </div>
                             <div className="section-customize">
-                                <div className="bg-image section-specialty"></div>
+                                <div className="bg-image section-handbook"></div>
                                 <div>Cơ Xương khớp 6</div>
                             </div>
                         </Slider>
@@ -65,4 +64,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(HandBook);
