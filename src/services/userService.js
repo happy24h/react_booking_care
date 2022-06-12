84d0c0f2bdc9a35a043264cea887b7ddea2e9c71
@anhnv99 <vietanhhappy99@gmail.com>
2022-06-12 12:36:54 +0700

    lesson 99

--- src/services/userService.js
+++ src/services/userService.js
@@ -92,6 +92,10 @@ const createNewClinic = (data) => {
     return axios.post('/api/create-new-clinic', data)
 }
 
+const getAllPatientForDoctor = (data) => {
+    return axios.get(`/api/get-list-patient-for-doctor?doctorId=${data.doctorId}&date=${data.date}`)
+}
+
 
 export { 
     handleLoginApi, getAllUsers, 
@@ -103,5 +107,6 @@ export {
     getProfileDoctorById, postPatientBookAppointment,
     postVerifyBookAppointment, createNewSpecialty,
     getAllSpecialty,getAllDetailSpecialtyById,
-    createNewClinic, getAllClinic, getAllDetailClinicById
+    createNewClinic, getAllClinic, 
+    getAllDetailClinicById, getAllPatientForDoctor
 }
